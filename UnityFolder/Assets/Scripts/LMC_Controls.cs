984fb0a42d24ffbf59ab104ea75c3cc78ef89337
Sagar Patel <17.sagar.patel@gmail.com>
2014-03-03 18:41:02 +0900

    Basic LMC fingertip input working

---
+++ UnityFolder/Assets/Scripts/LMC_Controls.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using Leap;
+
+
+public class LMC_Controls : MonoBehaviour 
+{
+	Controller controller;
+
+	public GameObject pointerPrefab;
+
+	GameObject pointer;
+
+	// Use this for initialization
+	void Start () 
+	{
+		// Leap stuff
+		controller = new Controller();
+		controller.EnableGesture(Gesture.GestureType.TYPECIRCLE);
+		controller.Config.SetFloat("Gesture.Circle.MinArc", Mathf.PI );
+		controller.Config.Save();	
+
+		pointer = (GameObject) GameObject.Instantiate(pointerPrefab, transform.position, Quaternion.identity);
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		Frame currentFrame = controller.Frame();
+
+		DisplayFingerTips(currentFrame);
+	}
+
+	void DisplayFingerTips(Frame frame)
+	{
+
+		Pointable pointable = frame.Pointables.Frontmost;
+
+		Vector3 tipPosition = pointable.StabilizedTipPosition.ToUnity();
+
+		pointer.transform.position = tipPosition;
+
+		Debug.Log(tipPosition);
+
+	}
+
+}
